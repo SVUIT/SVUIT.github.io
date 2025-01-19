07aea6f1563fb9385978190af0b11139688dd4e0
nquynh06 <truongnhuquynh2k6@gmail.com>
2025-01-19 16:05:12 +0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,53 +1,5 @@
 gsap.registerPlugin(ScrollTrigger);
 
-const balls = document.querySelectorAll('.image-ball1, .image-ball2, .image-ball3, .image-ball4, .image-ball5, .image-ball6, .image-ball7, .image-ball8, .image-ball9');
-const positionsBall = [
-    { x1: 0, y1: 50, x2: 0, y2: 100, x3: 0, y3: 150, x4: 0, y4: 200, x5: 0, y5: 250, x6: 0, y6: 300, x7: 0, y7: 350, x8: 0, y8: 400, x9: 0, y9: 450 },
-    { x1: 0, y1: 400, x2: 0, y2: 500, x3: 0, y3: 600, x4: 0, y4: 700, x5: 0, y5: 800, x6: 0, y6: 900, x7: 0, y7: 1000, x8: 0, y8: 1100, x9: 0, y9: 1200 },
-    { x1: 0, y1: 400, x2: 0, y2: 500, x3: 0, y3: 600, x4: 0, y4: 700, x5: 0, y5: 800, x6: 0, y6: 900, x7: 0, y7: 1000, x8: 0, y8: 1100, x9: 0, y9: 1200 },
-    { x1: 0, y1: 400, x2: 0, y2: 500, x3: 0, y3: 600, x4: 0, y4: 700, x5: 0, y5: 800, x6: 0, y6: 900, x7: 0, y7: 1000, x8: 0, y8: 1100, x9: 0, y9: 1200 },
-    { x1: 0, y1: 400, x2: 0, y2: 500, x3: 0, y3: 600, x4: 0, y4: 700, x5: 0, y5: 800, x6: 0, y6: 900, x7: 0, y7: 1000, x8: 0, y8: 1100, x9: 0, y9: 1200 },
-    { x1: 0, y1: 400, x2: 0, y2: 500, x3: 0, y3: 600, x4: 0, y4: 700, x5: 0, y5: 800, x6: 0, y6: 900, x7: 0, y7: 1000, x8: 0, y8: 1100, x9: 0, y9: 1200 },
-    { x1: 0, y1: 400, x2: 0, y2: 500, x3: 0, y3: 600, x4: 0, y4: 700, x5: 0, y5: 800, x6: 0, y6: 900, x7: 0, y7: 1000, x8: 0, y8: 1100, x9: 0, y9: 1200 },
-    { x1: 0, y1: 400, x2: 0, y2: 500, x3: 0, y3: 600, x4: 0, y4: 700, x5: 0, y5: 800, x6: 0, y6: 900, x7: 0, y7: 1000, x8: 0, y8: 1100, x9: 0, y9: 1200 },
-    { x1: 0, y1: 400, x2: 0, y2: 500, x3: 0, y3: 600, x4: 0, y4: 700, x5: 0, y5: 800, x6: 0, y6: 900, x7: 0, y7: 1000, x8: 0, y8: 1100, x9: 0, y9: 1200 },
-
-];
-
-balls.forEach((imageball, index) => {
-    const position = positionsBall[index];
-
-    gsap.timeline({
-        scrollTrigger: {
-            trigger: imageball,
-            start: "top center",
-            end: "bottom top",
-            scrub: true,
-            toggleActions: "play none none none",
-            onUpdate: (self) => {
-                if (self.progress >= 0.5) { 
-                    gsap.to(imageball, {
-                        x: position.x1, y: position.y1, duration: 1 
-                    });
-                } else {
-                    gsap.to(imageball, {
-                        x: position.x2, y: position.y2, duration: 1 
-                    });
-                }
-            }
-        }
-    })
-    .fromTo(imageball, { x: 0, y: 0, scale: 1, opacity: 1 }, { x: position.x1, y: position.y1, duration: 1 })
-    .to(imageball, { x: position.x2, y: position.y2, duration: 1 })
-    .to(imageball, { x: position.x3, y: position.y3, duration: 1 })
-    .to(imageball, { x: position.x4, y: position.y4, duration: 1 })
-    .to(imageball, { x: position.x5, y: position.y5, duration: 1 })
-    .to(imageball, { x: position.x6, y: position.y6, duration: 1 })
-    .to(imageball, { x: position.x7, y: position.y7, duration: 1 })
-    .to(imageball, { x: position.x8, y: position.y8, duration: 1 })
-    .to(imageball, { x: position.x9, y: position.y9, duration: 1 });
-});
-
 const features = document.querySelectorAll('.feature');
 const positionsFeature = [
     { x1: -2000, y1: 0},
@@ -83,4 +35,25 @@ ellipses.forEach((ellipse) => {
         { scale: 1, opacity: 1 },
         { scale: 10, opacity: 1, duration: 0.2 });
 });
+
+const ellipse3 = document.querySelectorAll('.ellipse3');
+const positionsEllipse3 = [
+    { x1: 0, y1: -200},
+];
+
+ellipse3.forEach((ellipse3, index) => {
+    const position = positionsEllipse3[index];
+
+    gsap.timeline({
+        scrollTrigger: {
+            trigger: ellipse3,
+            start: "top -10%",
+            end: "bottom -50%",
+            scrub: true,
+        }
+    })
+    .fromTo(ellipse3, { x: 0, y: 0, scale: 1, opacity: 1 }, { x: position.x1, y: position.y1, duration: 100})
+});
+
+
 
